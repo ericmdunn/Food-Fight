e09191222ae3dadb069dd4f4acda60f64a79330d
ericmdunn <ericmdunn@gmail.com>
2021-05-28 13:35:36 -0400

    Finished through lecture 104

--- Scenes/Characters/Character.gd
+++ Scenes/Characters/Character.gd
@@ -1,11 +1,28 @@
 extends KinematicBody
 
 
+var food_types = {}
+var can_fire = true
+
 const PROJECTILE_SPEED = 50
 
 
+func _ready():
+	food_types = FileGrabber.get_files("res://Projectiles/Food_Types/")
+	print(food_types)
+	randomize()
+
+
+func try_to_fire():
+	if can_fire == true:
+		fire()
+		can_fire = false
+		$Timer.start()
+
+
 func fire():
-	var projectile = load("res://Projectiles/ProjectileTemplate.tscn").instance()
+	var random_food = food_types[randi() % food_types.size()]
+	var projectile = load(random_food).instance()
 	add_child(projectile)
 	projectile.set_as_toplevel(true)
 	projectile.global_transform = $Forward.global_transform
@@ -13,5 +30,7 @@ func fire():
 	projectile.linear_velocity = character_forward * PROJECTILE_SPEED
 
 
+func _on_Timer_timeout():
+	can_fire = true
 
 
