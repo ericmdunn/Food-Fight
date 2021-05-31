74220c35ee738ae05c07ff036aa0caa7f6a0e18a
ericmdunn <ericmdunn@gmail.com>
2021-05-31 12:58:40 -0400

    Finished through lecture 107

---
+++ GUI/PauseMenu.gd
@@ -0,0 +1,28 @@
+extends CanvasLayer
+
+
+func _input(event):
+	if Input.is_action_just_pressed("pause"):
+		get_tree().paused = not get_tree().paused
+		toggle_pause_menu()
+
+
+func toggle_pause_menu():
+	if get_tree().paused:
+		$Popup.show()
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	else:
+		$Popup.hide()
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+
+func _on_FullscreenButton_pressed():
+	OS.window_fullscreen = not OS.window_fullscreen
+
+
+func _on_CustomizeButton_pressed():
+	pass # Replace with function body.
+
+
+func _on_QuitButton_pressed():
+	get_tree().quit()
